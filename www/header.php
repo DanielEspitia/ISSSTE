29cb9b3107182faa0d16140d532d1329ab67c1bc
Daniel Espitia Rico <der_ca92@hotmail.com>
2014-07-22 12:17:12 -0500

    Header con menú de prueba
    Tomé el contenido del machote del profe y los css y los adapté en el
    header

--- www/header.php
+++ www/header.php
@@ -3,91 +3,93 @@
 
 <!--Logo issste-->
 
-     <div class="container-fluid">
+<div class="container-fluid">
         <div class="row"> <!--Renglon con logo issste-->
         <div class="col-md-1"></div>
-        <div class="col-md-7"><img src="http://localhost/Residencias/ISSSTE/www/imagenes/isssteheader.jpg" alt=""  > </div> 
+        <div class="col-md-7"><img src="http://localhost/ISSSTE/www/imagenes/isssteheader.jpg" alt=""  > </div> 
          </div>
+         <div class="col-md-4">
         </div> </div>
-   
-         <!--Menu-->
-      <div class="container-fluid">  
-        <div class="row"> <!-- Renglon con Menu -->
-        <div class="col-md-1"></div>
-        <div class="col-md-10">
-            
-        <div class="navbar navbar-inverse">
-        <div class="navbar-inner">
-            
-        <div class="bs-example">
-        <ul class="nav nav-pills">
-   
-      <li class="dropdown">
-        <a id="drop4" role="button" data-toggle="dropdown" href="#">DERECHOHABIENTES <span class="caret"></span></a>
-        <ul id="menu1" class="dropdown-menu" role="menu" aria-labelledby="drop4">
-            <li role="presentation"><a role="menuitem" tabindex="-1" href="pru.php">AFILIACION</a></li>
-          <li role="presentation"><a role="menuitem" tabindex="-1" href="">ESTANCIAS INFATILES</a></li>
-          <li role="presentation"><a role="menuitem" tabindex="-1" href="t">PENSIONADOS</a></li>
-
+     
+     <div class="col-md-1"></div>
+<div class="col-md-10"> <div id="encabezado">
+ <div class="menu_principal">
+      <div align="left">
+        <ul>
+          <li><a href="index.html">D<span class="letra_menu">erechohabientes</span></a>
+              <ul>
+                <li><a href="http://www2.issste.gob.mx:8080/index.php/derechohabientes/afiliacion-vigencia">Afiliación</a></li>
+                <li><a href="http://www2.issste.gob.mx:8080/index.php/derechohabientes/atencion-derechohabiente">Atención al Derechohabiente</a></li>
+                <li><a href="http://www2.issste.gob.mx:8080/index.php/derechohabientes/cultura-deporte">Cultura, Deporte y Recreación </a></li>
+            <li><a href="http://www2.issste.gob.mx:8080/index.php/derechohabientes/estancias">Estancias</a></li>
+                <li><a href="http://www2.issste.gob.mx:8080/index.php/derechohabientes/pensionados">Pensionados </a></li>
+                <li><a href="http://www.pensionissste.gob.mx/">PENSIONISSSTE</a></li>
+                <li><a href="http://www2.issste.gob.mx:8080/index.php/derechohabientes/prestamos">Préstamos</a></li>
+                <li><a href="http://sgm.issste.gob.mx/medica/index.php">Salud</a></li>
+                <li><a href="http://www.issste.gob.mx/turissste/index.html">Turismo</a></li>
+				<li><a href="http://www.fovissste.gob.mx/wb/">Vivienda</a></li>
+              </ul>
+          </li>
+          <li><a href="index.html">T<span class="letra_menu">odo el Público</span></a>
+              <ul>
+                <li><a href="http://www2.issste.gob.mx:8080/index.php/todo-el-publico/issste-a-a-comprar">El ISSSTE va a comprar</a></li>
+                <li><a href="http://www2.issste.gob.mx:8080/index.php/todo-el-publico/funerarias">Funerarias</a></li>
+                </li><a href="http://www.prevenissste.gob.mx/">Prevenissste cerca de tí</a></li>
+				<li><a href="http://www2.issste.gob.mx:8080/index.php/todo-el-publico/issstehuixtla">Centros Recreativos y Culturales ISSSTEHUIXTLA</a></li>
+                </ul>
+          </li>
+          <li><a href="index.html">S<span class="letra_menu">ervicios en Línea</span></a>
+              <ul>
+                <li><a href="http://oficinavirtual.issste.gob.mx/">Mi Oficina Virtual</a></li>
+                <li><a href="http://www2.issste.gob.mx:8080/index.php/2012-11-14-18-49-30/2012-11-14-18-50-35">Agenda TU Cita Médica</a></li>
+                <li><a href="http://www.issste.gob.mx/issstenet/index.html">Comprobantes de Pago a Pensionistas</a></li>
+                <li><a href="http://issstenet.issste.gob.mx/cgi353/fglccgi/wa/r/menu_sp">Consulta TU información</a></li>
+                <li><a href="http://www.issste.gob.mx/servicios/curp.html">Consulta en imprime tu CURP</a></li>
+                <li><a href="http://www2.issste.gob.mx:8080/index.php/2012-11-14-18-49-30/servs-serica">Pago de Cuotas y Aportaciones</a></li>
+                <li><a href="http://oficinavirtual.issste.gob.mx/">Préstamos Personales</a></li>
+                <li><a href="http://www.issste.gob.mx/servicios/existenciademedicamentos.html">Inventario de Médicamentos y Mat. de Curación</a></li>
+                <li><a href="http://www.issste.gob.mx/issstenet/index.html">ver más servicios</a></li>
+              </ul>
+          </li>
+          <li><a href="index.html">I<span class="letra_menu">nstituto</span></a>
+              <ul>
+                <li><a href="http://www2.issste.gob.mx:8080/index.php/instituto/quienes-somos">¿Quiénes Somos?</a></li>
+                <li><a href="http://www2.issste.gob.mx:8080/index.php/instituto/admin01">Administraciónl</a></li>
+                <li><a href="http://www2.issste.gob.mx:8080/index.php/instituto/comunicacion-social">Sala de Prensa</a></li>
+                <li><a href="http://www2.issste.gob.mx:8080/index.php/instituto/finanzas01">Finanzas</a></li>
+                <li><a href="http://www2.issste.gob.mx:8080/index.php/instituto/subdireccion">Innovación y Calidad</a></li>
+                <li><a href="http://www.issste.gob.mx/cmt/index.html">Medicina del Trabajo</a></li>
+                <li><a href="http://www.issste.gob.mx/instituto/normateca.html">Normateca</a></li>
+				<li><a href="http://www2.issste.gob.mx:8080/index.php/instituto/organos-gobierno">Órganos de Gobierno</a></li>
+				<li><a href="http://www2.issste.gob.mx:8080/index.php/instituto/menu-inst-oic">Organo Interno de Control</a></li>
+				<li><a href="http://www2.issste.gob.mx:8080/index.php/instituto/2012-11-14-19-16-37">Relaciones Internacionales</a></li>
+                <li><a href="http://www.issste.gob.mx/intranet">Intranet</a></li>
+              </ul>
+          </li>
+          <li><a href="index.html">I<span class="letra_menu">SSSTEduca e </span>I<span class="letra_menu">nvestiga</span></a>
+              <ul>
+                <li><a href="http://www2.issste.gob.mx:8080/images/downloads/derechohabientes/catalogodedescuento.pdf">Capacitación Institucional</a></li>
+                <li><a href="http://sgm.issste.gob.mx/medica/index.php">Convocatorias Áreas Médicas</a></li>
+                <li><a href="http://edn.issste.gob.mx/">Esc. Dietética y Nutrición</a></li>
+                <li><a href="http://www2.issste.gob.mx:8080/index.php/2012-11-14-19-17-11/2012-11-14-19-18-30">Formación Internacional y Especializada</a></li>
+                <li><a href="http://sgm.issste.gob.mx/medica/ensenanza.php">Formación de Rec. Humanos en Salud</a></li>
+				<li><a href="http://sgm.issste.gob.mx/medica/index.php">Investigación en áreas médicas</a></li>
+				<li><a href="http://www2.issste.gob.mx:8080/index.php/2012-11-14-19-17-11/minvestigacion-cacym">Cosejo Asesor Científico y Médico del ISSSTE</a></li>
+              </ul>
+          </li>
+          <li><a href="index.html">C<span class="letra_menu">ontáctanos</span></a>
+              <ul>
+                <li><a href="http://www.issste.gob.mx/opina/index.html">Comentarios y Sugerencias</a></li>
+                <li><a href="http://www2.issste.gob.mx:8080/index.php/contactanos/dir-inst">Directorios Institucionales</a></li>
+                <li><a href="http://www2.issste.gob.mx:8080/index.php/contactanos/issste-tel">ISSSTE-Tel|Orientación e Información</a></li>
+                <li><a href="http://sgm.issste.gob.mx/medica/issstemergencias.php">ISSSTE-Emergencias | Urgencias Médicas en el DF y Área Metrop.</a></li>
+                <li><a href="http://www.issste.gob.mx/contacto/quejas.html">Quejas y Denuncias OIC</a></li>
+              </ul>
+          </li>
         </ul>
-      </li>
-      <li class="dropdown">
-        <a id="drop5" role="button" data-toggle="dropdown" href="#">TODO EL PUBLICO <span class="caret"></span></a>
-        <ul id="menu2" class="dropdown-menu" role="menu" aria-labelledby="drop5">
-          <li role="presentation"><a role="menuitem" tabindex="-1" href="">Action</a></li>
-          <li role="presentation"><a role="menuitem" tabindex="-1" href="">Another action</a></li>
-          <li role="presentation"><a role="menuitem" tabindex="-1" href="">Something else here</a></li>
-          <li role="presentation" class="divider"></li>
-          
-        </ul>
-      </li>
-      <li class="dropdown">
-        <a id="drop6" role="button" data-toggle="dropdown" href="#">SERVICIOS EN LINEA<span class="caret"></span></a>
-        <ul id="menu3" class="dropdown-menu" role="menu" aria-labelledby="drop6">
-          <li role="presentation"><a role="menuitem" tabindex="-1" href="">Action</a></li>
-          <li role="presentation"><a role="menuitem" tabindex="-1" href="">Another action</a></li>
-          <li role="presentation"><a role="menuitem" tabindex="-1" href="">Something else here</a></li>
-          
-        </ul>
-      </li>
-      
-      <li class="dropdown">
-        <a id="drop6" role="button" data-toggle="dropdown" href="#">INSTITUTO<span class="caret"></span></a>
-        <ul id="menu3" class="dropdown-menu" role="menu" aria-labelledby="drop6">
-          <li role="presentation"><a role="menuitem" tabindex="-1" href="">Action</a></li>
-          <li role="presentation"><a role="menuitem" tabindex="-1" href="">Another action</a></li>
-          <li role="presentation"><a role="menuitem" tabindex="-1" href="">Something else here</a></li>
-          
-        </ul>
-      </li>
-      
-      <li class="dropdown">
-        <a id="drop6" role="button" data-toggle="dropdown" href="#">TRANSPARENCIA<span class="caret"></span></a>
-        <ul id="menu3" class="dropdown-menu" role="menu" aria-labelledby="drop6">
-          <li role="presentation"><a role="menuitem" tabindex="-1" href="">Action</a></li>
-          <li role="presentation"><a role="menuitem" tabindex="-1" href="">Another action</a></li>
-          <li role="presentation"><a role="menuitem" tabindex="-1" href="">Something else here</a></li>
-          
-        </ul>
-      </li>
-
-  <li class="dropdown">
-        <a id="drop6" role="button" data-toggle="dropdown" href="#">CONTACTANOS<span class="caret"></span></a>
-        <ul id="menu3" class="dropdown-menu" role="menu" aria-labelledby="drop6">
-          <li role="presentation"><a role="menuitem" tabindex="-1" href="">Action</a></li>
-          <li role="presentation"><a role="menuitem" tabindex="-1" href="">Another action</a></li>
-          <li role="presentation"><a role="menuitem" tabindex="-1" href="">Something else here</a></li>
-          
-        </ul>
-      </li>
-
-
- 
-    </ul>  <!--nav nav pills -->
+      </div>
+    </div>
   </div>
-              
-         </div>
-   </div>
-   </div> 
-   </div><!--Termina renglon -->
- </div> <!-- Menu Contenedor-->
+  </div>
+  </div>
+  <div class="col-md-1"></div>
