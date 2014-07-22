2b42c44305f78466c1f762d34f04d78dcb96fc22
DavidAlva <david-01-15@hotmail.com>
2014-07-22 17:05:23 -0500

    Mis pruebas

---
+++ www/Pru/pru.php
@@ -0,0 +1,132 @@
+
+<html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        
+        <title>ISSSTE GUANAJUATO</title>
+        
+<!--       <link rel="stylesheet" href="layout.css"> 
+       <link rel="stylesheet" href="format.css">
+       <link rel="stylesheet" href="menu.css">
+       <link rel="stylesheet" href="form.css">
+       <link rel="stylesheet" href="images.css">
+       <link rel="stylesheet" href="jqueryJoomla.css">
+       <link rel="stylesheet" href="carrousel.css">  -->
+
+       <link rel="stylesheet" href="styles.css">
+	   <link rel="stylesheet" href="allmode-default.css">
+        <!--loading Bootstrap-->
+       <link rel="stylesheet" href="../bootstrap-3.2.0-dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../bootstrap-3.2.0-dist/css/bootstrap-theme.min.css">
+        <script src="../bootstrap-3.2.0-dist/js/jquery.js"></script>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.js"> </script>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"> </script>
+    </head>
+
+    <body>
+    <!--Este es un comentario-->
+	<div class ="container"><span class="glyphicon glyphicon-copyright-mark"></span></div>
+    <div class="container">
+    <div class="row">
+    <div class="col-md-2"></div>
+     <div class="col-md-4">
+      <button type="button" class="btn btn-primary">Primary</button>
+
+     </div></div></div>
+     
+     <div class="container"> 
+     <div id="carousel-example-captions" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carousel-example-captions" data-slide-to="0" class="active"></li>
+        <li class="" data-target="#carousel-example-captions" data-slide-to="1"></li>
+        <li class="" data-target="#carousel-example-captions" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="item active">
+          <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI5MDAiIGhlaWdodD0iNTAwIj48cmVjdCB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iIzc3NyIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjQ1MCIgeT0iMjUwIiBzdHlsZT0iZmlsbDojNzc3O2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjU2cHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+OTAweDUwMDwvdGV4dD48L3N2Zz4=" data-src="holder.js/900x500/auto/#777:#777" alt="900x500">
+          <div class="carousel-caption">
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </div>
+        </div>
+        <div class="item">
+          <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI5MDAiIGhlaWdodD0iNTAwIj48cmVjdCB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iIzY2NiIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjQ1MCIgeT0iMjUwIiBzdHlsZT0iZmlsbDojNjY2O2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjU2cHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+OTAweDUwMDwvdGV4dD48L3N2Zz4=" data-src="holder.js/900x500/auto/#666:#666" alt="900x500">
+          <div class="carousel-caption">
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+        </div>
+        <div class="item">
+          <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI5MDAiIGhlaWdodD0iNTAwIj48cmVjdCB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iIzU1NSIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjQ1MCIgeT0iMjUwIiBzdHlsZT0iZmlsbDojNTU1NTtmb250LXdlaWdodDpib2xkO2ZvbnQtc2l6ZTo1NnB4O2ZvbnQtZmFtaWx5OkFyaWFsLEhlbHZldGljYSxzYW5zLXNlcmlmO2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjkwMHg1MDA8L3RleHQ+PC9zdmc+" data-src="holder.js/900x500/auto/#555:#5555" alt="900x500">
+          <div class="carousel-caption">
+            <h3>Third slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#carousel-example-captions" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+      </a>
+      <a class="right carousel-control" href="#carousel-example-captions" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+      </a>
+    </div>
+      
+         
+     </div> 
+
+
+
+<!--MENUS-->
+<div id="container">
+<!-- Header -->
+  <div id="header">
+
+          <div id="menuTop">
+              <!-- Menu Top Module -->
+                            
+<ul class="menu" id="navTop">
+<li class="item-468 primera"><a href="http://www2.issste.gob.mx:8080/" class="tooltip_arriba">Inicio</a></li><li class="item-469"><a href="http://www2.issste.gob.mx:8080/index.php/movil" class="ext tooltip_arriba">Versión Móvil</a></li><li class="item-470"><a href="http://www2.issste.gob.mx:8080/index.php/2012-11-14-18-28-46" class="tooltip_arriba">Contacto</a></li><li class="item-471"><a href="http://www2.issste.gob.mx:8080/index.php/menutop-directorios-institucionales" class="tooltip_arriba">Directorios</a></li><li class="item-472"><a href="http://www2.issste.gob.mx:8080/index.php/welcome" class="tooltip_arriba">English</a></li><li class="item-473 ultima"><a href="http://www2.issste.gob.mx:8080/index.php/mapadelsitio" class="tooltip_arriba">Mapa del sitio</a></li></ul>
+
+                                <br class="clear">
+            </div>
+            
+             <div id="issste">
+                <!-- Logo Module -->
+                         
+
+<div class="custom">
+	<a href="../images/issste2013-header_2.jpg" class="tooltip_arriba"><img src="../images/issste2013-header_2.jpg" border="0" alt="Logo ISSSTE"></a></div>
+
+                          </div>
+    <!-- ENTRADA cuadro de búsqueda -->
+             <div id="search">
+                <div id="gobmx-searchbox-container-issste">
+                 <script type="text/javascript">_gmx.loadGobMXsearchAPI({container:'gobmx-searchbox-container-issste',client:'issste',newWindow:true});</script>
+               </div> 
+    </div>
+               <!-- <div id="search">
+                                </div> -->
+    <!-- termina ENTRADA cuadro de búsqueda -->
+          <br class="clear">
+    
+          <div id="menu">
+              <!-- Menu Module -->
+                          
+<ul class="menu" id="nav">
+<li class="item-475 deeper parent primera"><a href="http://www2.issste.gob.mx:8080/index.php#" class="tooltip_arriba">Derechohabientes</a><ul><li class="item-476 primera"><a href="http://www2.issste.gob.mx:8080/index.php/derechohabientes/afiliacion-vigencia" class="tooltip_arriba">Afiliación</a></li><li class="item-1498"><a href="http://www2.issste.gob.mx:8080/index.php/derechohabientes/infromacion-relevante" class="tooltip_arriba">Trámites</a></li><li class="item-477"><a href="http://www2.issste.gob.mx:8080/index.php/sad" target="_blank" class="ext tooltip_arriba">Atención al Derechohabiente</a></li><li class="item-478"><a href="http://www2.issste.gob.mx:8080/index.php/derechohabientes/cultura-deporte" class="tooltip_arriba">Cultura, Deporte y Recreación</a></li><li class="item-480"><a href="http://www2.issste.gob.mx:8080/index.php/derechohabientes/estancias" class="tooltip_arriba">Estancias Infantiles</a></li><li class="item-481"><a href="http://www2.issste.gob.mx:8080/index.php/derechohabientes/pensionados" class="tooltip_arriba">Pensionados</a></li><li class="item-482"><a href="http://www.pensionissste.gob.mx/" target="_blank" class="ext tooltip_arriba">PENSIONISSSTE</a></li><li class="item-483"><a href="http://www2.issste.gob.mx:8080/index.php/derechohabientes/prestamos" class="tooltip_arriba">Préstamos</a></li><li class="item-486"><a href="http://sgm.issste.gob.mx/" target="_blank" class="ext tooltip_arriba">Salud</a></li><li class="item-487"><a href="http://www2.issste.gob.mx:8080/index.php/turissste/" class="tooltip_arriba">Turismo</a></li><li class="item-488 ultima"><a href="http://www.fovissste.gob.mx/wb/" target="_blank" class="ext tooltip_arriba">Vivienda</a></li></ul></li><li class="item-489 deeper parent"><a href="http://www2.issste.gob.mx:8080/index.php#" class="tooltip_arriba">Todo el Público</a><ul><li class="item-490 primera"><a href="http://www2.issste.gob.mx:8080/index.php/todo-el-publico/issste-a-a-comprar" class="tooltip_arriba">El ISSSTE va a comprar</a></li><li class="item-491"><a href="http://www2.issste.gob.mx:8080/index.php/todo-el-publico/funerarias" class="tooltip_arriba">Funerarias</a></li><li class="item-492"><a href="http://www.prevenissste.gob.mx/" target="_blank" class="ext tooltip_arriba">Prevenissste cerca de tí</a></li><li class="item-493"><a href="http://www.superissste.gob.mx/" target="_blank" class="ext tooltip_arriba">SuperISSSTE</a></li><li class="item-905 ultima"><a href="http://www2.issste.gob.mx:8080/index.php/todo-el-publico/issstehuixtla" class="tooltip_arriba">Centro Recreativo y Cultural ISSSTEHUIXTLA</a></li></ul></li><li class="item-494 deeper parent"><a href="http://www2.issste.gob.mx:8080/index.php#" class="tooltip_arriba">Servicios en Línea</a><ul><li class="item-495 primera"><a href="http://oficinavirtual.issste.gob.mx/" target="_blank" class="ext tooltip_arriba">Mi Oficina Virtual</a></li><li class="item-496"><a href="http://www2.issste.gob.mx:8080/index.php/agenda-cita-medica" target="_blank" class="ext tooltip_arriba">Agenda TU Cita Médica</a></li><li class="item-497"><a href="http://www2.issste.gob.mx:8080/index.php/2012-11-14-18-49-30/servslinea-comppagopensionistas" class="tooltip_arriba">Comprobantes de Pago a Pensionistas</a></li><li class="item-498"><a href="http://issstenet.issste.gob.mx/cgi353/fglccgi/wa/r/menu_sp" target="_blank" class="ext tooltip_arriba">Consulta TU información</a></li><li class="item-499"><a href="http://www2.issste.gob.mx:8080/index.php/2012-11-14-18-49-30/serviciosenlinea-curp" target="_blank" class="tooltip_arriba">Consulta e imprime tu CURP</a></li><li class="item-500"><a href="http://www2.issste.gob.mx:8080/index.php/2012-11-14-18-49-30/servs-serica" class="tooltip_arriba">Pago de Cuotas y Aportaciones</a></li><li class="item-501"><a href="http://oficinavirtual.issste.gob.mx/" target="_blank" class="ext tooltip_arriba">Trámite de Préstamos Personales</a></li><li class="item-502"><a href="http://www.silodisa.com/ConteoPorUMU.aspx" target="_blank" class="ext tooltip_arriba">Inventario Medicamentos y Mat. de Curación</a></li><li class="item-503"><a href="http://www2.issste.gob.mx:8080/index.php#" class="tooltip_arriba">Ventanilla Electrónica de Servicios</a></li><li class="item-504 ultima"><a href="http://www2.issste.gob.mx:8080/index.php/2012-11-14-18-49-30/serviciosenlinea-issstenet-home" class="tooltip_arriba">Ver más servicios</a></li></ul></li><li class="item-505 deeper parent"><a href="http://www2.issste.gob.mx:8080/index.php#" class="tooltip_arriba">Instituto</a><ul><li class="item-506 primera"><a href="http://www2.issste.gob.mx:8080/index.php/instituto/quienes-somos" class="tooltip_arriba">¿Quiénes Somos?</a></li><li class="item-507"><a href="http://www2.issste.gob.mx:8080/index.php/instituto/admin01" class="tooltip_arriba">Administración</a></li><li class="item-508"><a href="http://www2.issste.gob.mx:8080/index.php/instituto/comunicacion-social-salaprensa" class="tooltip_arriba">Sala de Prensa</a></li><li class="item-509"><a href="http://www2.issste.gob.mx:8080/index.php/instituto/finanzas01" class="tooltip_arriba">Finanzas</a></li><li class="item-510"><a href="http://www2.issste.gob.mx:8080/index.php/instituto/subdireccion" class="tooltip_arriba">Innovación y Calidad</a></li><li class="item-511"><a href="http://www2.issste.gob.mx:8080/index.php/instituto/instituto-cmt" class="tooltip_arriba">Comité de Medicina del Trabajo</a></li><li class="item-484"><a href="http://www2.issste.gob.mx:8080/index.php/instituto/mprincipal-prevencion-riesgos-trabajo" class="tooltip_arriba">Seguridad y Salud en el Trabajo</a></li><li class="item-512"><a href="http://normateca.issste.gob.mx/index.asp?usuario=Publico&pin=Consultivo2013" target="_blank" class="ext tooltip_arriba">Normateca</a></li><li class="item-513"><a href="http://www2.issste.gob.mx:8080/index.php/instituto/organos-gobierno" class="tooltip_arriba">Órganos de Gobierno</a></li><li class="item-514"><a href="http://www2.issste.gob.mx:8080/index.php/instituto/menu-inst-oic" class="tooltip_arriba">Órgano Interno de Control</a></li><li class="item-515"><a href="http://www2.issste.gob.mx:8080/index.php/instituto/2012-11-14-19-16-37" target="_blank" class="tooltip_arriba">Relaciones Internacionales</a></li><li class="item-516 ultima"><a href="http://www.issste.gob.mx/intranet/" target="_blank" class="ext tooltip_arriba">Intranet</a></li></ul></li><li class="item-524 deeper parent"><a href="http://www2.issste.gob.mx:8080/index.php/transparencia" class="tooltip_arriba">Transparencia</a><ul><li class="item-525 primera"><a href="http://www2.issste.gob.mx:8080/index.php/transparencia/normatividad-transparencia" class="tooltip_arriba">Normatividad</a></li><li class="item-526"><a href="http://www2.issste.gob.mx:8080/index.php/transparencia/comite-informacion" class="tooltip_arriba">Comité de Información</a></li><li class="item-527"><a href="http://www2.issste.gob.mx:8080/index.php/transparencia/transparencia-focalizada" class="tooltip_arriba">Transparencia Focalizada</a></li><li class="item-528"><a href="http://www2.issste.gob.mx:8080/index.php/transparencia/indicadores-programas" class="tooltip_arriba">Indicadores de Programas Presupuestarios</a></li><li class="item-529"><a href="http://www2.issste.gob.mx:8080/index.php/transparencia/recomendaciones-al-issste" class="tooltip_arriba">Recomendaciones emitidas por la CNDH, INMUJERES y CONAPRED</a></li><li class="item-530"><a href="http://www2.issste.gob.mx:8080/index.php/transparencia/estudios-opiniones" class="tooltip_arriba">Estudios y Opiniones</a></li><li class="item-532"><a href="http://www2.issste.gob.mx:8080/index.php/transparencia/rendicion-cuentas" class="tooltip_arriba">Rendición de Cuentas</a></li><li class="item-533 ultima"><a href="http://www2.issste.gob.mx:8080/index.php/transparencia/participacion-ciudadana" class="tooltip_arriba">Participación Ciudadana</a></li></ul></li><li class="item-517 deeper parent"><a href="http://www2.issste.gob.mx:8080/index.php#" class="tooltip_arriba">ISSSTEduca e Investiga</a><ul><li class="item-518 primera"><a href="http://www2.issste.gob.mx:8080/images/downloads/derechohabientes/catalogodedescuento.pdf" target="_blank" class="tooltip_arriba">Descuentos en Colegiaturas</a></li><li class="item-519"><a href="http://sgm.issste.gob.mx/medica/convocatorias.php" target="_blank" class="ext tooltip_arriba">Convocatorias Áreas Médicas</a></li><li class="item-520"><a href="http://edn.issste.gob.mx/" target="_blank" class="ext tooltip_arriba">Esc. de Dietética y Nutrición</a></li><li class="item-521"><a href="http://www2.issste.gob.mx:8080/index.php/2012-11-14-19-17-11/2012-11-14-19-18-30" target="_blank" class="tooltip_arriba">Formación Internacional Especializada</a></li><li class="item-522"><a href="http://sgm.issste.gob.mx/medica/ensenanza.php" target="_blank" class="ext tooltip_arriba">Formación de Rec. Humanos en Salud</a></li><li class="item-523"><a href="http://sgm.issste.gob.mx/medica/index.php" target="_blank" class="ext tooltip_arriba">Investigación en Áreas Médicas</a></li><li class="item-1197 ultima"><a href="http://www2.issste.gob.mx:8080/index.php/2012-11-14-19-17-11/minvestigacion-cacym" class="tooltip_arriba">Consejo Asesor Científico y Médico del ISSSTE</a></li></ul></li><li class="item-534 deeper parent ultima"><a href="http://www2.issste.gob.mx:8080/index.php#" class="tooltip_arriba">Contáctanos</a><ul><li class="item-535 primera"><a href="http://www2.issste.gob.mx:8080/index.php/contactanos/2012-11-14-19-25-18" target="_blank" class="tooltip_arriba">Comentarios y Sugerencias</a></li><li class="item-536"><a href="http://www2.issste.gob.mx:8080/index.php/contactanos/dir-inst" class="tooltip_arriba">Directorios Institucionales</a></li><li class="item-537"><a href="http://www2.issste.gob.mx:8080/index.php/contactanos/issste-tel" class="tooltip_arriba">ISSSTE-Tel | Orientación e Información</a></li><li class="item-538"><a href="http://sgm.issste.gob.mx/medica/issstemergencias.php" target="_blank" class="ext tooltip_arriba">ISSSTE-Emergencias | Atención Médica Prehospitalaria</a></li><li class="item-539 ultima"><a href="http://www2.issste.gob.mx:8080/index.php/contactanos/2012-11-14-19-27-05" class="tooltip_arriba">Quejas y Denuncias al OIC</a></li></ul></li></ul>
+
+                                <br class="clear">            
+            </div>
+        
+  </div>             
+</div>
+
+<!-- Content -->
+<!--MENUS-->
+
+</body>
+</html>
