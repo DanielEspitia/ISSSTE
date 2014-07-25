d49ac0e3142d665e0bf84b18515cc65764b89b8c
Daniel Espitia Rico <der_ca92@hotmail.com>
2014-07-25 14:40:37 -0500

    Carpetas con plantilla

---
+++ www/Derechohabientes/Prestamos/plantilla.php
@@ -0,0 +1,67 @@
+<html lang="es">
+    
+    <head>
+    
+    <link href="images/logo.jpg" type="image/x-icon" rel="shortcut icon" />
+<link rel="alternate" type="application/rss+xml" title="Plantillas 2010" href="index.rss" />
+
+
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        
+        <title>ISSSTE GUANAJUATO- Trabajadores y Extrabajadores</title>
+        
+        <!--loading Bootstrap  -->
+        <link rel="stylesheet" href="../../bootstrap-3.2.0-dist/css/bootstrap.css">
+        <link rel="stylesheet" href="../../bootstrap-3.2.0-dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../../bootstrap-3.2.0-dist/css/bootstrap-theme.min.css">
+        <script src="../../bootstrap-3.2.0-dist/js/jquery.js"></script>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.js"> </script>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"> </script>
+		
+        <link rel="stylesheet" href="../../css/format.css">
+        <link rel="stylesheet" href="../../css/contenedores.css">
+        
+    
+    </head>
+
+    <body>
+
+     <?php
+         include('../../header2.php');
+       
+        ?>
+        
+
+        <!-- Contenedor Principal-->
+        <div class="container-fluid">
+        <div class="col-md-2"></div>
+        <div id="contentLeft">   <!-- Contenido PRincipal-->
+
+          
+        </div>   <!-- Fin contenido principal-->
+        
+               <div class="col-md-3">
+         <?php
+		 include('../../plantillaMenuD.php');
+		 ?>
+         
+         
+         </div>
+         
+        </div>
+    <!-- Cierre Contenedor Principal-->
+
+
+        
+      <?php
+         include('../../footer2.php');
+       
+        ?>
+   
+  
+    </body>
+
+</html>
