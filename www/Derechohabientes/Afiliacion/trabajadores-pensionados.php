dc2bb4801c1a407ec1a4436223bd58d82bdcba08
Daniel Espitia Rico <der_ca92@hotmail.com>
2014-07-25 13:01:27 -0500

    Avance Afiliacion

--- www/Derechohabientes/Afiliacion/trabajadores-pensionados.php
+++ www/Derechohabientes/Afiliacion/trabajadores-pensionados.php
@@ -50,22 +50,11 @@
         <br><br>
         
         
-        <p>
-
-Como Trabajador(a) o Pensionado(a) directo(a) responsable, ejerce el derecho que pertenece a tus Familiares Derechohabientes, registrándolos para que reciban atención médica inmediata y digna, a través de los servicios que el ISSSTE les ofrece.
-
-</p>
-<br>
-<p>
-Recuerda que este trámite es gratuito e indispensable para adquirir el carácter de Derechohabiente y tener acceso a las Clínicas y Unidades de Medicina Familiar. 
-</p>
- 
-        <br><br>
-        
-        <table class="table table-striped table-hover ">
+       
+  <table class="table table-striped table-hover ">
                 <tbody>
                   <tr>
-                    <td width="165">¿Cómo saber si soy Derechohabiente?</td>
+                    <td width="165"><b>¿Cómo saber si soy Derechohabiente?</b></td>
                     <td width="403">
                     <p >Solicite información :</p>
                     <p> A través de ISSSTE-TEL a los teléfonos 53 40 12 00, o 54 48 89 03 en el Distrito Federal y Área Metropolitana. En el resto del País sin costo al 01 800 71 04 666 , ó  consulte por Internet la página del ISSSTE www.issste.gob.mx en la Oficina Virtual</p>
@@ -75,35 +64,132 @@ Recuerda que este trámite es gratuito e indispensable para adquirir el carácte
                   </tr>
                   
    <tr>
-                    <td width="165">¿Dónde consulto mis datos personales registrados?</td>
+                    <td width="165"><b>¿Dónde consulto mis datos personales registrados?</b></td>
                     <td width="403">
                     <p >Esta información está disponible en la Oficina Virtual dentro del Expediente Electrónico Único.</p>
                     </td>
                   </tr>
  
  <tr>
-                    <td width="165">¿Dónde consulto mis datos laborales y el tiempo que he cotizado?</td>
+                    <td width="165"><b>¿Dónde consulto mis datos laborales y el tiempo que he cotizado?</b></td>
                     <td width="403">
                     <p> Esta información está disponible en la Oficina Virtual dentro del Expediente Electrónico Único y está conformada por los datos que identifican a la Dependencia o Entidad y el sueldo básico de cotización, así como, los periodos de cotización certificados derivados del registro de los movimientos afiliatorios proporcionados por las Dependencias y Entidades incorporadas, donde laboró el Trabajador.</p>
                     </td>
                   </tr>
 
    <tr>
-                    <td width="165">¿Dónde consulto los datos de mis Familiares Derechohabientes registrados?</td>
+                    <td width="165"><b>¿Dónde consulto los datos de mis Familiares Derechohabientes registrados?</b></td>
                     <td width="403">
                     <p >Esta información está disponible en la Oficina Virtual dentro del Expediente Electrónico Único.</p>
                     </td>
                   </tr>
                   
     <tr>
-                    <td width="165">¿Dónde puedo consultar mi domicilio registrado?</td>
+                    <td width="165"><b>¿Dónde puedo consultar mi domicilio registrado?</b></td>
+                    <td width="403">
+                    <p >Esta información está disponible en la Oficina Virtual dentro del Expediente Electrónico Único.</p>
+
+                    </td>
+                  </tr>
+
+    <tr>
+                    <td width="165"><b>¿Dónde puedo consultar la Clínica o Unidad de Medicina Familiar que me corresponde?</b></td>
+                    <td width="403">
+                    <p >Esta información está disponible en la Oficina Virtual dentro del Expediente Electrónico Único.</p>
+                   <p> La Clínica o Unidad de Medicina Familiar se asigna con base en el Código Postal del  domicilio particular registrado ante el Instituto por parte del Trabajador, Pensionado, extrabajador en continuación voluntario o extrabajador en conservación de derechos.</p>
+                    </td>
+                  </tr>
+                  
+<tr>
+                    <td width="165"><b>¿Dónde puedo consultar mi Régimen de Pensión?</b>	</td>
                     <td width="403">
                     <p >Esta información está disponible en la Oficina Virtual dentro del Expediente Electrónico Único.</p>
                     </td>
                   </tr>
-       
-                </tbody>
-              </table>
+                  
+<tr>
+                    <td width="165"><b>¿Quién tiene derecho al Servicio Médico?</b></td>
+                    <td width="403">
+                    <p ><b>Trabajador, Pensionado directo, extrabajador en conservación de derechos y extrabajador en continuación voluntaria (art. 41 Ley del ISSSTE):</b></p>
+                    <ul type="disc">
+                       <li>Esposa o Esposo</li>
+                       <li>Concubina ó Concubinario:</li>
+                       <li>Con hijo(a) procreado(a), adoptado(a) o reconocido(a)</li>
+                       <li>Sin hijo(a).</li>
+                       <li>Hijos:</li>
+                             <ul type="circle">
+                             <li>Hijo(a) menor de 18 años de edad.</li>
+                             <li>Hijo(a) soltero(a) estudiante de nivel medio superior, mayor de 18 años y hasta  cumplir 26 años.</li>
+                             <li>Hijo(a) incapacitado(a) física o psíquicamente mayor de 18 años de edad.</li>
+                             </ul>
+                      <li>Ascendientes:</li>
+                            <ul type="circle">
+                               <li>Padres, Abuelos, Bisabuelos etc.</li>
+                            </ul>
+                    </ul>
+                    
+                    <p><b>Pensionista indirecto:</b></p>
+                    
+                    <p>Tiene derecho a los servicios médicos, durante el tiempo que le sea otorgada la Pensión por:</p>
+                    
+                    <ul type="disc">
+                    <li>Viudez</li>
+                    <li>Orfandad</li>
+                    <li>Ascendencia</li>
+                    </ul>
+                    
+                    <p><b>No tiene derechos a la inscripción de Familiares.</b></p>               
+                    </td>
+                  </tr>
+                  </tbody>
+                  </table>
+                  
+                  <table class="table table-striped table-hover ">
+                <tbody>
+                  <tr align="center">
+                    <td colspan="2" id="centrar"><b>Requisitos</b></td>
+                  </tr>
+                 
+<tr>
+                    <td width="165"><b>¿Cómo registrarme ante el ISSSTE?</b></td>
+                    <td width="403">
+                    <p ><b>Trabajdor</b></p>
+                    <p>La Dependencia o Entidad tiene la obligación de registrarlo ante el ISSSTE y entregarle copia de su Aviso de Alta ante el Instituto.</p>
+                    
+                    <p><b>Pensionado</b></p>
+                    <p>La Subdirección de Pensiones realizará su registro.</p>
+                    
+                    <p><b>Extrabajador en Continuación Voluntaria:</b></p>
+                    
+                    <p>Después de prestar sus servicios en alguna Dependencia o Entidad y no tenga la calidad de Pensionado, podrá solicitar la continuación voluntaria en todos o alguno de los seguros del régimen obligatorio, con excepción del seguro de riesgos del trabajo. Para el caso del seguro de salud se requerirá que el Trabajador acredite haber laborado, cuando menos, cinco años en alguna Dependencia o Entidad incorporada al Instituto (art. 200 de la Ley del ISSSTE). <b> Sólo tiene sesenta días hábiles después de la baja para realizar el trámite.</b></p>
+                    
+                   <p>Al respecto, deberá realizar el trámite en el Departamento de Afiliación y Prestaciones Económicas de la Delegación del ISSSTE más cercana a su domicilio, con los siguientes documentos probatorios (original y copia):</p>
+                   
+                   <ul type="disc">
+                   <li>Último recibo de pago de sueldo.</li>
+                   <li>En caso de no tener registrada toda su historia laboral en la base de datos Única de derechohabientes, deberá presentar sus Hoja(s) Única(s) de Servicios o recibos de pago, para demostrar los cinco años de cotización al Instituto.</li>
+                   <li>Comprobante del domicilio actual de no más de tres meses:</li>
+                         <ul type="circle">
+                         <li>Recibo de energía eléctrica (Luz)</li>
+                         <li>Recibo de teléfono</li>
+                         <li>Estado de cuenta bancaria o comercial</li>
+                         <li>Contrato de arrendamiento</li>
+                         <li>Predial</li>                         
+                         </ul>
+                   <li>Identificación oficial:</li>
+                          <ul type="circle">
+                          <li>Credencial del IFE</li>
+                          <li>Pasaporte vigente Cédula Profesional</li>
+                          </ul>                   
+                   </ul>
+                    </td>
+                  </tr>
+
+                  
+</tbody>
+</table>
+
+          
         </div>
         </div>
     <!-- Cierre Contenedor Principal-->
