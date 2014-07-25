d7e67e1b03863fcbf739d6b5b4b34540e4d234b3
Daniel Espitia Rico <der_ca92@hotmail.com>
2014-07-25 16:11:30 -0500

    Avance Afiliacion

--- www/Derechohabientes/Afiliacion/requisitos-inscripcion.php
+++ www/Derechohabientes/Afiliacion/requisitos-inscripcion.php
@@ -24,7 +24,7 @@
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"> </script>
 		
         <link rel="stylesheet" href="../../css/format.css">
-        <link rel="stylesheet" href="contenedores.css">
+        <link rel="stylesheet" href="../../contenedores.css">
         
     
     </head>
@@ -41,34 +41,261 @@
         <div class="container-fluid">
         <div class="col-md-2"></div>
         <div id="contentLeft">
+        
+        <div id="text">
+        
+        
 
         <h1>Afiliación y Vigencia de Derechos</h1>  
 
         <br><br>
        
       
-        <h2>Orientación de Servicios y Trámites de Afiliación y Vigencia de Derechos Ante el ISSSTE</h2>
+        <h2>Requisitos para la Inscripción de Derechohabientes</h2>
  
-        <br><br>
+        <br>
         
-        
-        <p>
-
-Como Trabajador(a) o Pensionado(a) directo(a) responsable, ejerce el derecho que pertenece a tus Familiares Derechohabientes, registrándolos para que reciban atención médica inmediata y digna, a través de los servicios que el ISSSTE les ofrece.
-
-</p>
-<br>
-<p>
-Recuerda que este trámite es gratuito e indispensable para adquirir el carácter de Derechohabiente y tener acceso a las Clínicas y Unidades de Medicina Familiar. 
-</p>
- 
-        <br><br>
-         </div>
+       <strong>Trabajadores:</strong>
+       <br>
+       <br>
+       <p>El derecho a los seguros, servicios y prestaciones que la Ley del Instituto establece para sus afiliados, se inicia con la designación legal o nombramiento del trabajador en la Dependencia o Entidad que lo contrata y se extingue con la baja del trabajador en el servicio o al vencer el período de conservación de derechos (Artículo 21 del Reglamento de Afiliación, Vigencia de Derechos y Cobranza)</p>
+       
+       <p> Cuando el Instituto no reciba de la Dependencia o Entidad el Aviso de Alta de un Trabajador en el plazo señalado, podrá admitir la solicitud de afiliación directa del Trabajador, bien sea en el Departamento de Afiliación y Prestaciones Económicas de la Delegación o en la Clínica de Medicina Familiar que le corresponda de acuerdo a su domicilio. Para este fin, el Trabajador deberá presentar el último comprobante de sueldo o la constancia laboral y comprobante de domicilio actual de no más de tres meses (Credencial de Elector, Recibo de teléfono, Recibo de energía eléctrica, Predial, Estado de cuenta bancario o comercial, o contrato de arrendamiento.</p>
+       
+       <br>
+       
+       <strong>Pensionado:</strong>
+       <br>
+       
+       <p>El derecho de los seguros, servicios y prestaciones del Pensionado, se inicia cuando la Ley del ISSSTE le reconozca tal carácter y se extingue cuando pierde esa calidad (Artículo 22 del Reglamento de Afiliación, Vigencia de Derechos y Cobranza).</p>
+       
+      <br>
+      
+      <strong>FAMILIARES DERECHOHABIENTES:</strong>
+      <br>
+      
+      <p>Para que los Familiares Derechohabientes perciban las prestaciones que les corresponden, deberán cumplir los requisitos que la Ley y los Reglamentos del ISSSTE establezcan. El derecho de los Familiares Derechohabientes del Trabajador o del Pensionado directo, persiste mientras exista el derecho de éstos y conserven las condiciones establecida en el Artículo 41 de la Ley del ISSSTE . </p>
+      
+      <p>El Trabajador, Pensionado, extrabajador en conservación de derechos, extrabajador en continuación voluntaria puede registrar como sus Familiares Derechohabientes, a:</p>
+      
+      <blockquote>
+      <ul type="disc">
+        <li>Esposa o Concubina</li>
+        <li>Esposo o Concubinario</li>
+        <li>Hijos</li>
+        <li>Ascendientes (padres, abuelos, bisabuelos, etc.)</li>
+      </ul>
+      </blockquote>
+      
+      <br>
+      
+      <strong>REGISTRO DE FAMILIARES DERECHOHABIENTES:</strong>
+      <br><br>
+      <p>El trámite de registro de Familiares Derechohabientes puede ser realizado por:</p>
+      <blockquote>
+       <ul type="disc">
+        <li>El Trabajador</li>
+        <li>El Pensionado directo</li>
+        <li>El extrabajador en conservación de derechos</li>
+        <li>El extrabajador (a) en continuación voluntaria</li>
+        <li>El Familiar Derechohabiente</li>
+        <li>El Representante acreditado, mediante Carta Poder simple</li>
+        <li>El registro de la Concubina(rio) o de los hijos de Cónyuge sólo puede ser realizado por el propio Trabajador o Pensionado directo.  </li>
+      </ul>
+      </blockquote>
+      
+      <br>
+      
+      <strong>REGISTRO DE ESPOSA O ESPOSO:</strong><br><br>
+      <strong>Requisitos:</strong><br>
+      <blockquote>
+      <ul type="disc">
+         <li>Clave Única de Registro de Población (CURP)</li>
+         <li>Copia certificada del Acta de Nacimiento, reconocimiento o adopción</li>
+         <li>Copia certificada del Acta de Matrimonio, sin importar que la fecha de expedición exceda los seis meses</li>
+      </ul>
+      </blockquote>
+      <br>
+      
+      <strong>CONCUBINA O CONCUBINARIO SIN HIJO PROCREADO, ADOPTADO O RECONOCIDO CON EL TRABAJADOR O PENSIONADO DIRECTO:</strong><br><br>
+      <strong>Requisitos:</strong><br>
+      <blockquote>
+      <ul type="disc">
+         <li>Clave Única de Registro de Población (CURP)</li>
+         <li>Copia certificada del Acta de Nacimiento, reconocimiento o adopción de la (del) concubina(rio)</li>
+         <li>Información testimonial en la que se acredite que ha vivido en unión libre con el (la) Trabajador (a), extrabajador (a) en continuación voluntaria o en conservación de derechos o Pensionado (a) directo (a), al menos los últimos cinco años y que durante ese tiempo, ambos han permanecido libres de matrimonio</li>
+      </ul>
+      </blockquote>
+      <br>
+      <p>La información testimonial podrá ser certificada por las siguientes autoridades:</p>
+            <blockquote>
+      <ul type="disc">
+         <li>Juez de lo Familiar</li>
+         <li>Juez de Paz</li>
+         <li>Juez Cívico</li>
+         <li>Presidente Municipal</li>
+         <li>Agente Municipal</li>
+         <li>Notario Público</li>
+      </ul>
+      </blockquote>
+      <br>
+      
+      <strong>CONCUBINA O CONCUBINARIO CON HIJO (A) PROCREADO (A), ADOPTADO (A) O RECONOCIDO (A) CON EL (LA) TRABAJADOR (A) O PENSIONADO (A) DIRECTO (A):</strong>
+      <br><br>
+      <strong>Requisitos</strong>
+      <br>
+      <blockquote>
+      <ul type="disc">
+         <li>Clave Única de Registro de Población (CURP)</li>
+         <li>Copia certificada del Acta de Nacimiento, reconocimiento o adopción de la (del) concubina(rio)</li>
+         <li>Copia certificada del Acta de Nacimiento, reconocimiento o adopción del (de la) hijo (a)</li>
+      </ul>
+      </blockquote>
+      <br>
+      
+      <strong>HIJO (A) MENOR DE 18 AÑOS DE TRABAJADOR (A) O EXTRABAJADOR (A) EN CONTINUACIÓN VOLUNTARIA O EN CONSERVACIÓN DE DERECHOS O PENSIONADO (A) DIRECTO (A) O DE LA (DEL) CÓNYUGE:
+</strong>
+      <br><br>
+      <strong>Requisitos</strong>
+      <br>
+      <blockquote>
+      <ul type="disc">
+         <li>Clave Única de Registro de Población (CURP)</li>
+         <li>Copia certificada del Acta de Nacimiento, reconocimiento o adopción del hijo (a)</li>
+      </ul>
+      </blockquote>
+      <br>
+      
+       <strong>HIJO(A) SOLTERO (A), ESTUDIANTE DE NIVEL MEDIO SUPERIOR, MAYOR DE 18 AÑOS QUE AÚN NO HA CUMPLIDO 26, DE TRABAJADOR (A) O EXTRABAJADOR (A) EN CONTINUACIÓN VOLUNTARIA O EN CONSERVACIÓN DE DERECHOS O PENSIONADO (A) DIRECTO (A) O DE LA (DEL) CÓNYUGE:</strong>
+      <br><br>
+      <strong>Requisitos</strong>
+      <br>
+      <blockquote>
+      <ul type="disc">
+         <li>Clave Única de Registro de Población (CURP)</li>
+         <li>Copia certificada del Acta de Nacimiento, reconocimiento o adopción del hijo (a)</li>
+         <li>Constancia de estudios expedida por un plantel oficial o reconocido, incluso del extranjero, en la que se acredite que realiza estudios de nivel medio superior o superior en el ciclo escolar en curso.</li>
+      </ul>
+      </blockquote>
+      <br>
+       
+       <strong>HIJO (A) INCAPACITADO MAYOR DE 18 AÑOS, QUE NO PUEDE TRABAJAR PARA OBTENER SU SUBSISTENCIA, DE TRABAJADOR (A) O EXTRABAJADOR (A) EN CONTINUACIÓN VOLUNTARIA O EN CONSERVACIÓN DE DERECHOS O PENSIONADO (A) DIRECTO (A) O DE LA (DEL) CÓNYUGE:</strong>
+      <br><br>
+      <strong>Requisitos</strong>
+      <br>
+      <blockquote>
+      <ul type="disc">
+         <li>Clave Única de Registro de Población (CURP)</li>
+         <li>Copia certificada del Acta de Nacimiento, reconocimiento o adopción del hijo (a)</li>
+         <li>Certificado en el que el área Médica del Instituto acredite la incapacidad del (de la) hijo (a) que se pretende registrar.</li>
+      </ul>
+      </blockquote>
+      <br>        
+      
+       <strong>TRATÁNDOSE DE HIJO (A) DE CÓNYUGE ADICIONALMENTE, SE REQUIERE:</strong>
+      <br><br>
+      <blockquote>
+      <ul type="disc">
+         <li>Clave Única de Registro de Población (CURP)</li>
+         <li>Copia certificada del Acta de Matrimonio.</li>
+         <li>Declaración bajo protesta de decir verdad que la persona que pretende registrarse es dependiente económico del (de la) Trabajador (a), extrabajador (a) en continuación voluntaria o en conservación de derechos, Pensionado (a) directo.</li>
+      </ul>
+      </blockquote>
+      <br>        
+      
+       
+       <strong>ASCENDIENTES (PADRES, ABUELOS, BISABUELOS, ETC.):</strong>
+      <br><br>
+      <strong>Requisitos</strong>
+      <br>
+      <blockquote>
+      <ul type="disc">
+         <li>Clave Única de Registro de Población (CURP)</li>
+         <li>Copia certificada del Acta de Nacimiento, reconocimiento o adopción del ascendiente</li>
+         <li>Copia certificada del Acta de Nacimiento, reconocimiento o adopción de quien general el derecho</li>
+         <li>En el caso de los abuelos, bisabuelos, etc., copia certificada de las actas de nacimiento, reconocimiento o adopción necesarias para identificar el parentesco de la persona que se requiere registrar con quien genera el derecho.</li>
+         <li>Declaración bajo protesta de decir verdad que la persona que pretende registrarse es dependiente económico del (de la) Trabajador (a), extrabajador (a) en continuación voluntaria o en conservación de derechos, Pensionado (a) directo (a)
+</li>
+      </ul>
+      </blockquote>
+      <br>
+      
+      <strong>SERVICIOS CONDICIONADOS DE ATENCIÓN OBSTÉTRICA:</strong><br><br>
+      
+      <p>La mujer Trabajadora, la Pensionada, la cónyuge del Trabajador o Pensionado o, en su caso, la conbubina de uno y otro, y la hija del Trabajador o Pensionado, soltera, menor de dieciocho años que dependa económicamente de éstos, tendrá derecho a la asistencia obstétrica necesaria a partir del día en que el Instituto certifique el estado de embarazo.</p>
+      
+      <p>Este servicio queda condicionado al compromiso del Trabajador o Pensionado directo de registrar al recién nacido como su Familiar Derechohabiente, dentro de un plazo de sesenta días naturales posteriores al nacimiento.</p>
+      
+      <p>De no cumplirse con el compromiso de registro, el Instituto ejecutará las acciones pertinentes para que el Trabajador o Pensionado directo cubra íntegramente el costo de los servicios prestados tanto a la mujer como al menor.</p>
+      
+      <br>
+      
+      <strong>Requisitos</strong>
+           <br>
+      <blockquote>
+      <ul type="disc">
+         <li>Clave Única de Registro de Población (CURP)</li>
+         <li>Copia certificada del Acta de Nacimiento de la mujer para quien se solicita el servicio condicionado de atención obstétrica.</li>
+         <li>Certificado de gravidez expedido por médico del ISSSTE.</li>
+         <li>Información testimonial en la que se acredite que ambos se encuentran libres de matrimonio o concubinato.</li>
+         <li>Declaración firmada por el Trabajador o Pensionado directo en la que reconoce como su hijo al producto concebido con la mujer y a presentar copia certificada del Acta de Nacimiento, señalando que en caso contrario pagará los gastos que se originen por el servicio prestado.</li>
+      </ul>
+      </blockquote>
+      <br> 
+      
+      <strong>SERVICIOS DE ATENCIÓN OBSTRÉTICA:</strong>
+      <br>
+      <p>Para que la Trabajadora, Pensionada, Esposa, Hija menor de 18 años y soltera, o en su caso, la concubina tengan derecho al servicio de atención obstétrica, será necesario que durante los seis meses anteriores al parto, se hayan mantenido vigentes los derechos como afiliados del Instituto</p>
+      <br>
+      <strong>Requisitos</strong>
+      <br><br>
+      <strong>Hija menor de 18 años registrada, que requiere el servicio obstétrico.</strong>
+      <br>
+      <blockquote>
+      <ul type="disc">
+         <li>Información testimonial en la que se acredite la dependencia económica y expresamente se precise que se encuentra libre de matrimonio y que no vive en concubinato.</li>
+      </ul>
+      </blockquote>
+      
+      <br><br>
+      <strong>Hija menor de 18 años no registrada, que requiere servicio obstétrico.</strong>
+      <br>
+      <blockquote>
+      <ul type="disc">
+         <li>Clave Única de Registro de Población (CURP)</li>
+         <li>Copia certificada del Acta de Nacimiento, adopción o de reconocimiento de hija.</li>
+         <li>Información testimonial en la que se acredita la dependencia económica y expresamente se precise que se encuentra libre de matrimonio y que no vive en concubinato.</li>
+      </ul>
+      </blockquote>
+       <br>      
+      
+      <strong>CONSTANCIAS DE VIGENCIA DE DERECHOS</strong>
+      <br>
+      <h3>La Constancia de Vigencia de Derecho sólo puede ser solicitadas por:</h3>
+      <br>
+       <blockquote>
+      <ul type="disc">
+         <li>Trabajador o Pensionado</li>
+         <li>Representante acreditado mediante carta poder simple</li>
+         <li>De acuerdo al poder otorgado por el Trabajador o Pensionado</li>
+         <li>La constancia puede incluir al solicitante y a su núcleo familiar.</li>
+         <li>Pensionado indirecto o Familiar Derechohabiente</li>
          
+      </ul>
+      </blockquote>
+      <p>La constancia sólo puede incluir al solicitante</p>
+      
+      
+              
+         </div>
+         </div>  <!-- contentLeft  -->
+         
+       <div class="col-md-2"></div>
          <?php
 		 include('../../plantillaMenuD.php');
 		 ?>
-         
+        </div>
+        
         </div>
     <!-- Cierre Contenedor Principal-->
 
