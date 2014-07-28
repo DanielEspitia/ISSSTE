be43c14eddbc0d9ba31edc8a386115571e38b16c
DavidAlva <david-01-15@hotmail.com>
2014-07-28 02:20:48 -0500

    Plantillas  con el menú a la derecha
    Hice copia de afiliacion.php para las demás carpetas para de ahí
    sustituir únicamente el contenido pero ese sería nuestro MACHOTE jaa!

---
+++ www/Derechohabientes/Prestamos/prestamos.php
@@ -0,0 +1,86 @@
+﻿<html lang="es">
+    
+    <head>
+    
+    <link href="../../images/logo.jpg" type="image/x-icon" rel="shortcut icon" />
+<link rel="alternate" type="application/rss+xml" title="Plantillas 2010" href="index.rss" />
+<!--[if IE 7]><link rel="stylesheet" type="text/css" href="css/estiloIE7.css" /><![endif]-->
+<!--[if IE]><link rel="stylesheet" type="text/css" href="css/estilo.css" /><![endif]-->
+
+
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        
+        <title>ISSSTE GUANAJUATO- Afiliaci&oacute;n</title>
+        
+        <!--loading Bootstrap  -->
+        <link rel="stylesheet" href="../../bootstrap-3.2.0-dist/css/bootstrap.css">
+        <link rel="stylesheet" href="../../bootstrap-3.2.0-dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../../bootstrap-3.2.0-dist/css/bootstrap-theme.min.css">
+        <script src="../../bootstrap-3.2.0-dist/js/jquery.js"></script>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.js"> </script>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"> </script>
+		
+        <link rel="stylesheet" href="../../css/format.css">
+        <link rel="stylesheet" href="../../css/contenedores.css">
+        
+    
+    </head>
+
+    <body>
+
+     <?php
+         include('../../header2.php');
+       
+        ?>
+        
+
+        <!-- Contenedor Principal-->
+        <div class="container-fluid">
+        <div class="col-md-2"></div>
+        <div class="col-md-5">
+
+        <h1>Afiliación y Vigencia de Derechos</h1>  
+
+        <br><br>
+       
+      
+        <h2>Orientación de Servicios y Trámites de Afiliación y Vigencia de Derechos Ante el ISSSTE</h2>
+ 
+        <br><br>
+        
+        
+        <p>
+
+Como Trabajador(a) o Pensionado(a) directo(a) responsable, ejerce el derecho que pertenece a tus Familiares Derechohabientes, registrándolos para que reciban atención médica inmediata y digna, a través de los servicios que el ISSSTE les ofrece.
+
+</p>
+<br>
+<p>
+Recuerda que este trámite es gratuito e indispensable para adquirir el carácter de Derechohabiente y tener acceso a las Clínicas y Unidades de Medicina Familiar. 
+</p>
+ 
+        <br><br>
+         </div>	 
+       <div class="col-md-3">
+         <?php
+		 include('../../plantillaMenuD.php');
+		 ?>
+         
+         
+         </div>
+        </div>
+    <!-- Cierre Contenedor Principal-->
+
+
+        
+      <?php
+         include('../../footer2.php');
+       
+        ?>
+   
+  
+    </body>
+
