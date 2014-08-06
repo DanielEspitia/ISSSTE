fafbda942085827a6ebbc1475bec4897f31c9526
Daniel Espitia Rico <der_ca92@hotmail.com>
2014-08-06 13:24:45 -0500

    Menu Noticias Optimizado

--- www/Derechohabientes/Estancias/menuEstancias.php
+++ www/Derechohabientes/Estancias/menuEstancias.php
@@ -30,41 +30,10 @@
                      
              <!-- Banner Module -->
                   
-            <!-- NewsRight Module -->
-               <div id="newsRight">
-                  		<div class="moduletableNews">
-					<h3>Noticias</h3>
-					<div class="allmode_box allmode-default News">
-
-
-
-<div class="allmode_itemsbox">
-
-	<div class="allmode_item">
-
-				<div class="allmode_img"><a class="tooltip_arriba" href="../../Instituto/Sala-de-Prensa/21-07-2014%20comsocial.php"><img src="../../images/tb.jpg" title="UN ISSSTE SIN REZAGO EDUCATIVO" alt="" height="60" width="80"></a></div>
-		
-		
-				<h4 class="allmode_title"><a class="tooltip_arriba" href="../../Instituto/Sala-de-Prensa/21-07-2014%20comsocial.php">UN ISSSTE SIN REZAGO EDUCATIVO </a></h4>
-		
-		
-		
-	</div>
-
-	<div class="allmode_item">
-
-				<div class="allmode_img"><a class="tooltip_arriba" href="../../Instituto/Sala-de-Prensa/19-07-2014%20comsocial.php"><img src="../../images/n2.jpg" title="ANUNCIA DIRECTOR GENERAL DEL ISSSTE DERRAMA ECONÓMICA POR MÁS DE 14 MMDP PARA EDOMEX" alt="" height="60" width="80"></a></div>
-		
-		
-				<h4 class="allmode_title"><a class="tooltip_arriba" href="../../Instituto/Sala-de-Prensa/19-07-2014%20comsocial.php">ANUNCIA DIRECTOR GENERAL DEL ISSSTE DERRAMA ECONÓMICA POR MÁS DE 14 MMDP PARA EDOMEX</a></h4>
-		
-		
-		
-	</div>
-</div>
-<div class="allmode_showall"><a class="tooltip_arriba" href="/index.php/instituto/comunicacion-social-salaprensa">Ver más comunicados</a></div></div>		</div>
-	
-           </div>
+                        <!-- NewsRight Module -->
+             	<?php
+         include('../../noticias.php');
+      ?>
                          
              <!-- Social Module -->
                   
---
+++ www/noticias.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Prueba de Menu Derecho</title>
+<link rel="stylesheet" href="../../menu/styles.css">
+<link rel="stylesheet" href="../../menu/linea#3.css">
+<link rel="stylesheet" href="../../menu/allmode-default.css">
+</head>
+
+<body>
+
+            <!-- NewsRight Module -->
+               <div id="newsRight">
+                  		<div class="moduletableNews">
+					<h3>Noticias</h3>
+					<div class="allmode_box allmode-default News">
+
+
+
+
+<div class="allmode_itemsbox">
+
+	<div class="allmode_item">
+
+				<div class="allmode_img"><a class="tooltip_arriba" href="../../Instituto/Sala-de-Prensa/21-07-2014%20comsocial.php"><img src="../../images/tb.jpg" title="UN ISSSTE SIN REZAGO EDUCATIVO" alt="" height="60" width="80"></a></div>
+		
+		
+				<h4 class="allmode_title"><a class="tooltip_arriba" href="../../Instituto/Sala-de-Prensa/21-07-2014%20comsocial.php">UN ISSSTE SIN REZAGO EDUCATIVO</a></h4>
+		
+		
+		
+	</div>
+
+	<div class="allmode_item">
+
+				<div class="allmode_img"><a class="tooltip_arriba" href="../../Instituto/Sala-de-Prensa/19-07-2014%20comsocial.php"><img src="../../images/n2.jpg" title="ANUNCIA DIRECTOR GENERAL DEL ISSSTE DERRAMA ECONÓMICA POR MÁS DE 14 MMDP PARA EDOMEX" alt="" height="60" width="80"></a></div>
+		
+		
+				<h4 class="allmode_title"><a class="tooltip_arriba" href="../../Instituto/Sala-de-Prensa/19-07-2014%20comsocial.php">ANUNCIA DIRECTOR GENERAL DEL ISSSTE DERRAMA ECONÓMICA POR MÁS DE 14 MMDP PARA EDOMEX</a></h4>
+		
+		
+		
+	</div>
+</div>
+<div class="allmode_showall"><a class="tooltip_arriba" href="/index.php/instituto/comunicacion-social-salaprensa">Ver más comunicados</a></div></div>		</div>
+	
+           </div>
+                         
+             <!-- Social Module -->
+                  
+             <!-- ISSSTEMERGENCIAS Module -->
+                    
+        </div>
+</body>
+</html>
