28a1bea3c778ac45c1d15c81cbbbc016b9724229
DavidAlva <david-01-15@hotmail.com>
2014-08-13 10:36:18 -0500

    Header, como estaba

--- www/index.php
+++ www/index.php
@@ -3,7 +3,7 @@
     
     <head>
     <link href="images/logo.jpg" type="image/x-icon" rel="shortcut icon" />
-<link rel="alternate" type="application/rss+xml" title="Plantillas 2010" href="index.rss" />
+	<link rel="alternate" type="application/rss+xml" title="Plantillas 2010" href="index.rss" />
 
 
         <meta charset="UTF-8">
