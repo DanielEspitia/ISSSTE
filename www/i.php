24495a25493176ecf5edfb2c71872d69c8127ba1
Roberto17 <robe_19cz@hotmail.com>
2014-08-20 15:43:44 -0500

    Modificacion slide de noticias

---
+++ www/i.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+    <head>
+    <link href="images/logo.jpg" type="image/x-icon" rel="shortcut icon" />
+	<link rel="alternate" type="application/rss+xml" title="Plantillas 2010" href="index.rss" />
+
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        
+        <title>ISSSTE GUANAJUATO</title>
+        
+        <!--loading Bootstrap  -->
+        <link rel="stylesheet" href="bootstrap-3.2.0-dist/css/bootstrap.css">
+        <link rel="stylesheet" href="bootstrap-3.2.0-dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="bootstrap-3.2.0-dist/css/bootstrap-theme.min.css">
+        <script src="bootstrap-3.2.0-dist/js/jquery.js"></script>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.js"> </script>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"> </script>
+		<link rel="stylesheet" href="css/format.css">
+        <link rel="stylesheet" href="css/contenedores.css">
+        <link rel="stylesheet" href="menu/styles.css">
+      	<link rel="stylesheet" type="text/css" href="engine1/style.css" />	<!--	css de wow slider	-->
+		<script type="text/javascript" src="engine1/jquery.js"></script>	<!--	css de wow slider	--> 
+		
+		<!--	Rotador de Noticias	-->
+		<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+		<script src="plugins/jquery.cycle.all.js"></script>
+		
+
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('#rotar').cycle({
+			fx:     'scrollUp',
+			timeout: 6000,
+			delay:  -2000
+		});
+	});    
+</script>
+<!--Rotador de Noticias-->
+    
+	</head>
+
+    <body>
+
+     <?php
+         include('header1.php');       
+        ?>
+        <!-- Carrusel-->
+	<!-- Start WOWSlider.com BODY section -->
+	<div id="wowslider-container1">
+	<div class="ws_images"><ul>
+<li><a href="Instituto/Sala-de-Prensa/05-08-2014 comsocial.php" title="Ver Noticia"><img src="data1/images/com_30_img_2.jpg" alt="El Presidente de la Corte Interamericana de Derechos Humanos visita el ISSSTE" title="El Presidente de la Corte Interamericana de Derechos Humanos visita el ISSSTE" id="wows1_0"/></a></li>	<!--	Noticia más reciente	-->
+<li><a href="Instituto/Sala-de-Prensa/21-07-2014 comsocial.php" title="Ver Noticia"><img src="data1/images/com_29_img_3.jpg" alt="Un ISSSTE sin rezago educativo" title="Un ISSSTE sin rezago educativo" id="wows1_1"/></a></li>
+<li><a href="Instituto/Sala-de-Prensa/19-07-2014 comsocial.php" title="Ver Noticia"><img src="data1/images/com_28_img_1.jpg" alt="Anuncia director general del ISSSTE derrama ecónomica por más de 14 MMDP para EDOMEX" title="Anuncia director general del ISSSTE derrama ecónomica por más de 14 MMDP para EDOMEX" id="wows1_2"/></a></li>
+<li><a href="Instituto/Sala-de-Prensa/17-07-2014 comsocial.php" title="Ver Noticia"><img src="data1/images/com_27_img_7.jpg" alt="Más de 1773 MDP para créditos de vivienda a derechohabientes en Veracruz: Lerdo de Tejada" title="Más de 1773 MDP para créditos de vivienda a derechohabientes en Veracruz: Lerdo de Tejada" id="wows1_3"/></a></li>
+<li><a href="Instituto/Sala-de-Prensa/16-07-2014 comsocial.php" title="Ver Noticia"><img src="data1/images/com_26_img_5.jpg" alt="Para Veracruz, el ISSSTE destina más de 9 mil 400 millones de pesos en 2014" title="Para Veracruz, el ISSSTE destina más de 9 mil 400 millones de pesos en 2014" id="wows1_4"/></a></li>
+</ul></div>
+<div class="ws_bullets"><div>
+<a href="#" title="El Presidente de la Corte Interamericana de Derechos Humanos visita el ISSSTE"><img src="data1/tooltips/com_30_img_2.jpg" alt="El Presidente de la Corte Interamericana de Derechos Humanos visita el ISSSTE"/>1</a>
+<a href="#" title="Un ISSSTE sin rezago educativo"><img src="data1/tooltips/com_29_img_3.jpg" alt="Un ISSSTE sin rezago educativo"/>2</a>
+<a href="#" title="Anuncia director general del ISSSTE derrama ecónomica por más de 14 MMDP para EDOMEX"><img src="data1/tooltips/com_28_img_1.jpg" alt="Anuncia director general del ISSSTE derramma ecónomica por más de 14 MMDP para EDOMEX"/>3</a>
+<a href="#" title="Más de 1773 MDP para créditos de vivienda a derechohabientes en Veracruz: Lerdo de Tejada"><img src="data1/tooltips/com_27_img_7.jpg" alt="Más de 1773 MDP para créditos de vivienda a derechohabientes en Veracruz: Lerdo de Tejada"/>4</a>
+<a href="#" title="Para Veracruz, el ISSSTE destina más de 9 mil 400 millones de pesos en 2014"><img src="data1/tooltips/com_26_img_5.jpg" alt="Para Veracruz, el ISSSTE destina más de 9 mil 400 millones de pesos en 2014"/>5</a>
+</div></div>
+	</div>
+	<script type="text/javascript" src="engine1/wowslider.js"></script>
+	<script type="text/javascript" src="engine1/script.js"></script>
+	<!-- End WOWSlider.com BODY section -->
+        <!--FIN Carrusel-->
+     <!--2° bloque-->
+        <div class="container">   
+		<div class="row"><div class="col-md-1"></div> <div class="col-md-3">
+		
+		<!--<div id="contentLeft">-->
+		<div id="contentLeft01">
+		<h3 align="center">Noticias</h3>
+					<br>
+
+<!-- ********************************************************************** -->
+ <div class="cycle-main" onmouseover="$('#rotar').cycle('pause');" onmouseout="$('#rotar').cycle('resume');">
+    	
+        <div id="rotar" class="cycle-content"> 
+
+			<div><a href="Instituto/Sala-de-Prensa/05-08-2014%20comsocial.php" class="tooltip_arriba">
+				<img src="images/photos/Comunicados2014/05-08-14/Com_1.jpg"  width="110" height="90" title="EL PRESIDENTE DE LA CORTE INTERAMERICANA DE DERECHOS HUMANOS VISITA EL ISSSTE " alt="" class="img-thumbnail"></a>
+				<br><br><h4 class="allmode_title" ><a href="Instituto/Sala-de-Prensa/05-08-2014%20comsocial.php" class="tooltip_arriba">
+				EL PRESIDENTE DE LA CORTE INTERAMERICANA DE DERECHOS HUMANOS VISITA EL ISSSTE </a></div>
+			
+			<div><a href="Instituto/Sala-de-Prensa/21-07-2014%20comsocial.php" class="tooltip_arriba">
+				<img src="images/tb.jpg"  width="110" height="90" title="UN ISSSTE SIN REZAGO EDUCATIVO" alt="" class="img-thumbnail"></a>
+				<br><br><h4 class="allmode_title" ><a href="Instituto/Sala-de-Prensa/21-07-2014%20comsocial.php" class="tooltip_arriba">
+				UN ISSSTE SIN REZAGO EDUCATIVO </a></div>
+			
+		
+        </div> 
+        
+    </div> 
+
+<!-- ********************************************************************** -->
+<br>
+<div class="allmode_showall"><a href="/index.php/instituto/comunicacion-social-salaprensa" class="tooltip_arriba">Ver más comunicados</a></div>		
+
+<br>
+	<div class="moduletable">
+			<h3>Obligaciones de Transparencia</h3>
+					
+
+	<br>
+
+<p align="left"><a href="http://portaltransparencia.gob.mx/buscador/search/search.do?method=begin" target="_blank" class="ext tooltip_arriba"><img src="images/portal-transparencia.jpg" class="img-thumbnail" border="0" title="Pulsa aquí para conocer la información que publica el ISSSTE (contratos, directorios, sueldos, etc.). Los servidores públicos responsables de la información publicada en el Portal de Obligaciones de Transparencia, certifican la veracidad de la misma." alt="Banner con liga al POT" width="270px"></a></p>
+
+
+<p align="left"><a href="https://www.infomex.org.mx/gobiernofederal/home.action" target="_blank" class="tooltip_arriba"><img src="images/banner_infomex.png" class="img-thumbnail" border="0" title="Acceso al Sistema Infomex" alt="Banner con liga al sistema INFOMEX" width="270px" ></a></p>
+
+
+<p align="left"><a href="http://inicio.ifai.org.mx/_catalogs/masterpage/ifai.aspx" target="_blank" class="ext tooltip_arriba"><img src="images/banner_ifai.png" class="img-thumbnail" border="0" title="Instituto Federal de Acceso a la Información y Protección de Datos" alt="Banner con liga al sitio del Instituto Federal de Acceso a la Información y Protección de Datos" width="270px" ></a></p>
+
+	</div>
+	</div>
+	
+         
+			
+			
+			  
+			  
+			
+			<!--Cierre</div> Contentleft de Noticias-->
+			 </div>  <!--fin col de Noticias-->
+		
+		<!--Información relevante-->
+		
+		<div class="col-md-4">
+		<div id="contentLeft02"> 
+                  		<!--<div class="moduletable">-->
+					<h3 align="center">Información Relevante </h3>
+					
+
+<div class="custom">
+	<div>
+<table width="352" border="0">
+<tbody>
+<tr class="alt primera ultima">
+
+<td>
+<p align="center"><strong>Estimado Pensionado:</strong></p>
+<br>
+<p align="justify">Nos satisface informarle que hemos transformado el pase de vigencia, <strong>eliminando la modalidad presencial, es decir, usted ya no debe trasladarse a ninguna oficina del ISSSTE a realizar el pase de vigencia</strong>, ahora el registro de la supervivencia la realizará el personal del ISSSTE, a través de diversas modalidades como son: Visitas domiciliarias aleatorias, y la validación de la CURP de los pensionados con la base de datos del Registro Nacional de Población (RENAPO). Lo invitamos a registrarse a la oficina virtual del ISSSTE, en: <a href="/images/redirect/oficinavirtual.html" target="_blank" class="tooltip_arriba"><strong><em>https://oficinavirtual.issste.gob.mx/</em></strong></a> si el sistema no le permite ingresar, debe presentar su CURP en la Subdelegación de Prestaciones más cercana a su domicilio. Mayor información ISSSTETEL (01800-710-4666) y 01800-pensión (01800-7367466) opción 2</p>
+</td>
+</tr>
+</tbody>
+</table>
+</div>
+<div class="info" >
+<p align="center"><a href="http://sgm.issste.gob.mx/medica/convocatorias.php" target="_blank" class="tooltip_arriba"><img src="images/ingreso-residencias-medicas.jpg" border="0" alt="Ingreso a Residencias Médicas" width="350" height="75" ></a></p>
+</div>
+<div class="info">
+<div align="center"><a href="http://www.pensionissste.gob.mx/" target="_blank" class="tooltip_arriba"><img src="images/estado-de-cuenta-SAR.jpg" border="0" alt="Donde consultar el estado de cuenta del SAR" width="350" height="75" ></a></div>
+</div>
+<div class="info">
+<p align="center"><a href="http://ebdis.issste.gob.mx/pubconsEBDI.asp" target="_blank" class="tooltip_arriba"><img src="images/directorio-estancias-infantiles.jpg" border="0" alt="Directorio de Estancias Infantiles" width="350" height="75" ></a></p>
+</div></div>
+		<!--</div>-->
+	
+         </div>
+		</div>
+			  
+	    <!--Fin Información relevante-->
+		
+	
+	<!--fin noticias-->
+		
+<div class="col-md-2">
+		
+	<a href="Derechohabientes/Afiliacion/agenda-cita-medica.php"><img src="images/agenda-cita.jpg" alt="agenda tu cita" width="280" height="80" class="img-rounded" ></a> <br><br>
+	<a href="http://www.fovissste.gob.mx/" target="_blank"><img src="images/fovissste.jpg" alt="fovissste" width="280" height="80"  class="img-rounded"></a> <br><br>
+	<a href="http://www.pensionissste.gob.mx/" target="_blank"><img src="images/pensionissste.jpg" alt="pensionissste" width="280" height="80" class="img-rounded" ></a> <br><br>
+    <a href="http://www2.issste.gob.mx:8080/index.php/turissste" target="_blank"><img src="images/turissste.jpg" alt="turissste" width="280" height="80" class="img-rounded"></a> <br><br>
+	<a href="http://www.superissste.gob.mx/" target="_blank"><img src="images/superissste.jpg" alt="superissste" width="280" height="80" class="img-rounded"></a> <br><br>
+	<a href="http://www2.issste.gob.mx:8080/index.php/2012-11-14-18-49-30/servs-serica" target="_blank"><img src="images/logoserica.jpg" alt="serica" width="280" height="80" class="img-rounded"></a> 	
+
+</div>
+		<div class="col-md-2"></div>
+		
+		</div><!-- Cierre row -->
+
+		</div><!-- Cierre Contenedor -->
+       
+
+
+
+
+        
+      <?php
+         include('footer1.php');
+       
+        ?>
+   
+    </body>
+</html>
