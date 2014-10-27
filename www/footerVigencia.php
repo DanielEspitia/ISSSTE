a3be8df846ce87fe8dc8178ed370f5198d72dc23
Daniel Espitia Rico <der_ca92@hotmail.com>
2014-10-27 14:55:26 -0600

    Arbol de archivos Vigencia
    Ya casi queda!! :+1

---
+++ www/footerVigencia.php
@@ -0,0 +1,38 @@
+<!-- footer -->
+   <div id="bodyCopyright">
+        <div id="socialFooter">
+
+	<a href="http://www.facebook.com/pages/ISSSTE/383477189617" target="_blank" class="ext tooltip_arriba"><img src="../images/facebook.png" alt="Facebook"></a>
+<a href="https://twitter.com/ISSSTEGuanajuat" target="_blank" class="tooltip_arriba"><img src="../images/twitter.png" alt="Twitter"></a>
+
+                    </div>
+
+        <div id="logoFooter">
+                  
+
+	<p><img src="../images/footer.jpg" alt=""></p>
+
+                    </div>
+        
+        <div id="copyright">
+                  
+
+
+<div class="col-md-6"> <font size="-2"> AV. FRANCISCO JUÁREZ S/N ESQ. AMERICAS, COL. LOS LAURELES, PLAZA XOCHIPILLI </font></div>
+<div class="col-md-2"></div>
+<div class="col-md-6"> <font size="-2">CONMUTADOR: (01-461) 609-36-91, 609-35-08, 609-21-66, 609-25-29 </font></div>
+
+<div class="col-md-6"> <font size="-2"> CELAYA, GUANAJUATO, MÉXICO</font></div>
+<div class="col-md-2"></div>
+<div class="col-md-6"> <font size="-2">(01-461) 614-51-64, 614-26-63, 614-26-69, 615-11-49 </font></div>
+
+<div class="col-md-12">
+<hr>
+</div>
+	<div class="col-md-12"><strong>ISSSTE, GUANAJUATO - ALGUNOS DERECHOS RESERVADOS © 2014</strong><p></p></div>
+
+
+                    </div>
+</div>
+
+<!-- footer -->
---
+++ www/headerVigencia.php
@@ -0,0 +1,40 @@
+<link rel="stylesheet" href="../menu/styles.css">
+<!--MENUS-->
+
+<div id="container">
+<!-- Header -->
+  <div id="header">
+
+          <div id="menuTop">
+        
+            <!-- Menu Top Module -->                            
+			<ul class="menu" id="navTop">
+				<li class="item-468 primera"><a href="../index.php" class="tooltip_arriba">Inicio</a></li>
+                 <li class="item-470"><a href="../Contactanos/Comentarios/comentarios-sugerencias.php" class="tooltip_arriba">Contacto</a></li>
+                <li class="item-471"><a href="../Contactanos/Directorios/directorios-institucionales.php" class="tooltip_arriba">Directorios</a></li>
+				<li class="item-472"><a href="../menuTop/Ubicanos/map.php" class="tooltip_arriba">Ubicanos</a></li>
+                <li class="item-473 ultima"><a href="#" class="tooltip_arriba">Mapa del sitio</a></li>
+			</ul>
+
+			<br class="clear"> 
+								
+            </div>
+            
+             <div id="issste">
+                <!-- Logo Module -->
+                         
+<div class="custom">
+<a href="http://www2.issste.gob.mx:8080" class="tooltip_arriba"><img id="logo_pos" src="../images/issste2013-header_2.jpg" border="" alt="Logo ISSSTE"></a></div>
+
+                          </div>
+
+          <br class="clear">
+
+        
+  </div>             
+</div>
+<br class="clear">
+<!-- Content -->
+<!--FIN MENUS-->
+
+
