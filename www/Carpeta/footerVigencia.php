5894d4fc990bc17d631e40f5cb6a30b963e14fcd
DavidAlva <david-01-15@hotmail.com>
2014-11-03 13:21:32 -0600

    pruebas de JAOFileTree

---
+++ www/Carpeta/footerVigencia.php
@@ -0,0 +1,23 @@
+<!-- footer -->
+ <div id="bodyCopyright">
+  
+    <div id="copyright">
+                  
+<div class="col-md-6"> <font size="-2"> AV. FRANCISCO JUÁREZ S/N ESQ. AMERICAS, COL. LOS LAURELES, PLAZA XOCHIPILLI </font></div>
+<div class="col-md-2"></div>
+<div class="col-md-6"> <font size="-2">EXTENSIONES VIGENCIA: 37005, 37033, 37044</font></div>
+
+<div class="col-md-6"> <font size="-2"> CELAYA, GUANAJUATO, MÉXICO</font></div>
+<div class="col-md-2"></div>
+<div class="col-md-6"> <font size="-2">(01-461) 614-51-64, 614-26-63, 614-26-69, 615-11-49 </font></div>
+
+<div class="col-md-12">
+<hr>
+</div>
+	<div class="col-md-12"><strong>ISSSTE, GUANAJUATO - ALGUNOS DERECHOS RESERVADOS © 2014</strong><p></p></div>
+
+
+     </div>
+</div>
+
+<!-- footer -->
---
+++ www/Carpeta/headerVigencia.php
@@ -0,0 +1,24 @@
+<link rel="stylesheet" href="../menu/styles.css">
+<!--MENUS-->
+
+<div id="container">
+<!-- Header -->
+  <div id="header">
+   <div id="issste">
+                <!-- Logo Module -->
+                         
+<div class="custom">
+<a href="http://www2.issste.gob.mx:8080" class="tooltip_arriba"><img id="logo_pos" src="../images/issste2013-header_2.jpg" border="" alt="Logo ISSSTE"></a></div>
+
+                          </div>
+
+          <br class="clear">
+
+        
+  </div>             
+</div>
+<br class="clear">
+<!-- Content -->
+<!--FIN MENUS-->
+
+
